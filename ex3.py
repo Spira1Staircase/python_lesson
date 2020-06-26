8850cae8f53ea9a37c640770592348ef8219368a
dmmkawarada <sasakama1984@gmail.com>
2020-06-26 16:36:36 +0900

    Finish ex3

---
+++ ex3.py
@@ -0,0 +1,17 @@
+#xの平方根を求める
+x = 2
+#
+rnew = x
+#
+diff = rnew - x/rnew
+if (diff < 0):
+    diff = diff
+while (diff > 1.0E-6):
+    r1 = rnew
+    r2 = x/r1
+    rnew = (r1 + r2)/2
+    print(r1, rnew, r2)
+    diff = r1 - r2
+    if (diff < 0):
+        diff = -diff
+        
