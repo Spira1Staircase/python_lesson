5b4301a02096323589d14d4084988e3fc7b3a03d
dmmkawarada <sasakama1984@gmail.com>
2020-06-26 15:57:11 +0900

    finish ex1

---
+++ ex1.py
@@ -0,0 +1,24 @@
+#xの平方根を求める
+x = 2
+#
+rnew = x
+#
+r1 = rnew
+r2 = x/r1
+rnew = (r1 + r2)/2
+print(r1,rnew,r2)
+#
+r1 = rnew
+r2 = x/r1
+rnew = (r1 + r2)/2
+print(r1, rnew, r2)
+#
+r1 = rnew
+r2 = x/r1
+rnew = (r1 + r2)/2
+print(r1, rnew, r2)
+#
+r1 = rnew
+r2 = x/r1
+rnew = (r1 + r2)/2
+print(r1, rnew, r2)
