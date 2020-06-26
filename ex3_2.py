699874e452675d91a27ac9288b17bd4c7fe2ea77
dmmkawarada <sasakama1984@gmail.com>
2020-06-26 16:39:43 +0900

    Finish ex3_2

---
+++ ex3_2.py
@@ -0,0 +1,12 @@
+x = 2
+rnew = x
+while True:
+    r1 = rnew
+    r2 = x/r1
+    rnew = (r1 + r2)/2
+    print(r1, rnew, r2)
+    diff = r1 - r2
+    if (diff < 0):
+        diff = -diff
+    if diff <= 1.0E-6:
+        break
