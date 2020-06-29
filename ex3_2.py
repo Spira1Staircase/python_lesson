10f91b520e7a77f9e444d4a8292ab99328c03e0f
dmmkawarada <sasakama1984@gmail.com>
2020-06-29 17:06:30 +0900

    Finish test2

--- ex3_2.py
+++ ex3_2.py
@@ -5,9 +5,9 @@ while True:
     r1 = rnew
     r2 = x/r1
     rnew = (r1 + r2)/2
-    print(r1, rnew, r2)
     diff = r1 - r2
     if (diff < 0):
         diff = -diff
     if diff <= 1.0E-6:
         break
+print(rnew)
