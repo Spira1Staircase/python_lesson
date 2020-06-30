58a0b4857e2fd99266843aa2f775896e5e1d2c0b
dmmkawarada <sasakama1984@gmail.com>
2020-06-30 10:15:17 +0900

    Finish turtle2

---
+++ turtle2.py
@@ -0,0 +1,17 @@
+from turtle import *
+n = 5
+for i in range(n):
+
+#  forward(d): d だけ前進。fd(d) も同じ。
+#  back(d), bk(d), backword(d): 後退
+#  right(a), rt(a): 右へ a 度回転
+#  left(a), lt(a) :左へ a 度回転
+#  goto(x, y), setpos(x, y), setposition(x, y): 座標 x, y に移動
+#  setheading(a): 向きを a 度に設定
+#  pendown(), pd(), down(): 軌跡を描くペンを下ろします。
+#  penup(), pu(), up(): ペンを上げます
+#  position(), pos(): タートルの位置を２次元ベクトルとして返します。
+#  以下のように2つの変数で返り値を受け取ります。
+#  x, y = pos()
+#  heading(): タートルの向きを返します
+#  isdown(): ペンが降りていれば True を、上がっていれば False を返します。
