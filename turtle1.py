5760b97670ad846d728b7bc9f4cdfa9f53ca0b82
dmmkawarada <sasakama1984@gmail.com>
2020-06-30 11:08:41 +0900

    Update turtle1

--- turtle1.py
+++ turtle1.py
@@ -4,11 +4,11 @@ begin_fill()
 while True:
     forward(100)
     left(90)
-    forword(100)
+    forward(100)
     left(90)
-    forword(100)
+    forward(100)
     left(90)
-    forword(100)
+    forward(100)
     left(90)
     if abs(pos()) < 1:
         break
