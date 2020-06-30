d485072dde244d5d0a363542b6247eb4f59fcf7a
dmmkawarada <sasakama1984@gmail.com>
2020-06-30 10:52:03 +0900

    Update turtle1,3

--- turtle1.py
+++ turtle1.py
@@ -1,10 +1,16 @@
-# from turtle import*
-# forword(100)
-# left(90)
-# forword(100)
-# left(90)
-# forword(100)
-# left(90)
-# forword(100)
-# left(90)
-# done()
+from turtle import *
+color('red', 'yellow')
+begin_fill()
+while True:
+    forward(100)
+    left(90)
+    forword(100)
+    left(90)
+    forword(100)
+    left(90)
+    forword(100)
+    left(90)
+    if abs(pos()) < 1:
+        break
+end_fill()
+done()
